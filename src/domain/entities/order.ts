2c7a35f946854c97aa64ac858ecee1ced491134f
Guilherme Henrique Crozariol <guicrozariol@gmail.com>
2024-05-14 11:35:00 -0300

    feat(entity): add order entity

---
+++ src/domain/entities/order.ts
@@ -0,0 +1,74 @@
+import { Entity } from '@/core/entities/entity'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { Optional } from '@/core/types/optional'
+
+export enum Status {
+  'READY_FOR_PICKUP' = 'READY_FOR_PICKUP',
+  'DELIVERED' = 'DELIVERED',
+  'RETURNED' = 'RETURNED',
+}
+
+interface OrderProps {
+  recipientId: UniqueEntityID
+  description: string
+  address: string
+  status: Status
+  pickedUpAt?: Date
+  deliveredAt?: Date
+  returnedAt?: Date
+  createdAt: Date
+}
+
+export class Order extends Entity<OrderProps> {
+  static create(
+    props: Optional<OrderProps, 'createdAt' | 'status'>,
+    id?: UniqueEntityID,
+  ) {
+    const order = new Order(
+      {
+        ...props,
+        status: Status.READY_FOR_PICKUP,
+        createdAt: new Date(),
+      },
+      id,
+    )
+
+    return order
+  }
+
+  get recipientId() {
+    return this.props.recipientId
+  }
+
+  get description() {
+    return this.props.description
+  }
+
+  get address() {
+    return this.props.address
+  }
+
+  get status() {
+    return this.props.status
+  }
+
+  set status(status: Status) {
+    this.props.status = status
+  }
+
+  get createdAt() {
+    return this.props.createdAt
+  }
+
+  get pickedUpAt() {
+    return this.props.pickedUpAt
+  }
+
+  get deliveredAt() {
+    return this.props.deliveredAt
+  }
+
+  get returnedAt() {
+    return this.props.returnedAt
+  }
+}
