e560139d23c7531e9ec8ad730501ecf1a8f62125
Guilherme Henrique Crozariol <guicrozariol@gmail.com>
2024-06-10 11:14:13 -0300

    feat(errors): create functional error handling + refactor delete courier use cases & tests

---
+++ src/core/either.spec.ts
@@ -0,0 +1,27 @@
+import { Either, left, right } from './either'
+
+function doSomething(success: boolean): Either<string, number> {
+  if (success) {
+    return right(10)
+  }
+
+  return left('error')
+}
+
+test('success result', () => {
+  const result = doSomething(true)
+
+  expect(result.isLeft()).toEqual(false)
+  expect(result.isRight()).toEqual(true)
+
+  expect(result.value).toBeTypeOf('number')
+})
+
+test('error result', () => {
+  const result = doSomething(false)
+
+  expect(result.isLeft()).toEqual(true)
+  expect(result.isRight()).toEqual(false)
+
+  expect(result.value).toBeTypeOf('string')
+})
