d3967a0da12968c932e1b0a3d06df8e8cbecfb5b
Guilherme Henrique Crozariol <guicrozariol@gmail.com>
2024-06-10 11:20:44 -0300

    refactor: delete order use case + unit tests

--- src/domain/carrier/application/use-cases/delete-order.spec.ts
+++ src/domain/carrier/application/use-cases/delete-order.spec.ts
@@ -2,6 +2,7 @@ import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { DeleteOrderUseCase } from './delete-order'
 import { makeOrder } from '@test/factories/make-order'
 import { InMemoryOrdersRepository } from 'test/repositories/in-memory/in-memory-orders-repository'
+import { ResourceNotFoundError } from './errors/resource-not-found-error'
 
 describe('Delete Order [USE CASE]', () => {
   it('should be able to delete an order', async () => {
@@ -12,8 +13,19 @@ describe('Delete Order [USE CASE]', () => {
 
     await inMemoryOrdersRepository.create(order)
 
-    await sut.execute({ orderId: 'order-1' })
+    const result = await sut.execute({ orderId: 'order-1' })
 
+    expect(result.isRight()).toEqual(true)
     expect(inMemoryOrdersRepository.items).toHaveLength(0)
+  })
+
+  it('should not be able to delete an inexistent order', async () => {
+    const inMemoryOrdersRepository = new InMemoryOrdersRepository()
+    const sut = new DeleteOrderUseCase(inMemoryOrdersRepository)
+
+    const result = await sut.execute({ orderId: 'inexistent-order-1' })
+
+    expect(result.isLeft()).toEqual(true)
+    expect(result.value).toBeInstanceOf(ResourceNotFoundError)
   })
 })
